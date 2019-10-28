aa1da469e46dbcd75f0461acb594342559525318
leox64 <55474957+leox64@users.noreply.github.com>
2019-10-27 22:20:41 -0600

    Add files via upload

---
+++ colas/cola.py
@@ -0,0 +1,11 @@
+"""
+Cola: Una cola es una estructura de datos lineal  que almacena datos manupulados con el
+protocolo fifo (first-in,first-out). Esto significa que los elmentos son insetados al
+final (enqueue) de la estructura, elminando elementos qiue se encuantran en la cabeza
+de la estructrura (dequeue).
+La estructrura cola tiene la siguientes operaciones: El constructor queue(), la función is_empty() que
+averigua si la cola está vacía, el método length() regresa el numero de elementos en la cola,
+el metodo enqueue()que recibe un elemento y lo agrega al final de la cola y el metodo dequeue()
+que elminia el elemento al principio de la cola y lo regresa.
+
+"""
---
+++ colas/colas con prioridad.py
@@ -0,0 +1,20 @@
+"""
+Colas de prioridad: Es una vesión extendida de la implementación de cola básica, pero con la
+variante de cada elemento insetado llevará un número que indica una prioridad especifica,
+los elmentos son ordenados en base a esa prioridad. Si hay más de un elemento con la misma
+prioridad estos s e menipulan con el protocolo "fifo".
+Cola doble: una cola doble es aquella en la que se puede agregar tanto al final como al
+inicio, de la misma forma se puedan sacar de principio y del final. El ADT es el siguiente:
+DoubleQueue()
+add_first(value)
+add_last(value)
+dequeue_first()
+dequeue_last()
+first()
+last()
+is_empty()
+length()
+to_string()
+
+"""
+## colas dobles
